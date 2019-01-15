9cdcb6b4e089574ca800b40b7293ea51aef0f774
Eric Crosson <eric.s.crosson@utexas.com>
2019-01-15 14:30:11 -0600

    Fully implement and test fromString

--- src/market-session.ts
+++ src/market-session.ts
@@ -4,6 +4,7 @@
  */
 
 import ow from 'ow'
+import { ArgumentError } from './argument-error'
 
 const defaultSessions = [
     '5', '15', '30', '60', '240', '720', '1440', '1H',
@@ -18,19 +19,61 @@ export interface Session {
 
 
 /**
- * DOCUMENT
+ * Convert a string-based representation of a market session into an
+ * integer describing the session length in minutes.
+ *
+ * Valid input consists of a number followed by one of the following
+ * suffixes: '', 'H', 'D', 'W', 'M', or 'Y'.
+ *
+ * A string consisting only of a valid suffix-character will be
+ * interpreted as having an implicit quantifier of 1.
  */
 function fromString(session: string): number {
     ow(session, ow.string.not.empty)
     ow(session, ow.string.matches(/^[1-9]?[0-9]*[HDWMY]?$/))
 
     let resolution: number = -1
+
     if (/^[1-9][0-9]*$/.test(session)) {
         resolution = parseInt(session)
     }
-
+    else if (/^[1-9][0-9]*H$/.test(session)) {
+        resolution = parseInt(session) * 60
+    }
+    else if (/^[1-9][0-9]*D$/.test(session)) {
+        resolution = parseInt(session) * 60 * 24
+    }
+    else if (/^[1-9][0-9]*W$/.test(session)) {
+        resolution = parseInt(session) * 60 * 24 * 7
+    }
+    else if (/^[1-9][0-9]*M$/.test(session)) {
+        // TODO: verify this is the number used in each TradingView calendar month
+        resolution = parseInt(session) * 60 * 24 * 7 * 30
+    }
+    else if (/^[1-9][0-9]*Y$/.test(session)) {
+        // TODO: verify this is the number used in each TradingView calendar year
+        resolution = parseInt(session) * 60 * 24 * 7 * 30 * 12
+    }
+    /* handle the implicit-1 scenario */
+    else if (/^H$/.test(session)) {
+        resolution = 60
+    }
+    else if (/^D$/.test(session)) {
+        resolution = 60 * 24
+    }
+    else if (/^W$/.test(session)) {
+        resolution = 60 * 24 * 7
+    }
+    else if (/^M$/.test(session)) {
+        resolution = 60 * 24 * 7 * 30
+    }
+    else if (/^Y$/.test(session)) {
+        resolution = 60 * 24 * 7 * 30 * 12
+    }
+    // Note: this block should never run. If you are seeing this
+    // error, the argument validation above is incorrect
     else {
-        // TODO: throw error here
+        throw new ArgumentError(`Cannot interpret session interval '${session}'`, fromString)
     }
 
     return resolution
@@ -56,16 +99,9 @@ const session = (date: Date, sessions: string[] = defaultSessions): number[] => 
 
 
 Object.defineProperties(session, {
-    // fromString: {
-    //     value: (session: string): number => {
-    //         return 5
-    //     }
-    // }
     fromString: {
         value: fromString
     }
-
-
     //,
     // toString: {
     //     value: toString
--- test/test-from-string.ts
+++ test/test-from-string.ts
@@ -13,7 +13,7 @@ test('should throw ArgumentError when given an empty string', t => {
     t.is(error.message, '[NOT] Expected string to be empty, got ``')
 })
 
-test('should throw ArgumentError when given non-sensical timeframes', t => {
+test('should throw ArgumentError when given nonsensical timeframes', t => {
     const testStrings = ['5B', '5d', 'p', 'D4', '!!', 'H4']
     testStrings.forEach((str) => {
         const error = t.throws(() => {
@@ -23,7 +23,6 @@ test('should throw ArgumentError when given non-sensical timeframes', t => {
     })
 })
 
-
 test('integers should return identity numbers', t => {
     t.is(5, session.fromString('5'))
     t.is(15, session.fromString('15'))
@@ -32,4 +31,34 @@ test('integers should return identity numbers', t => {
     t.is(1440, session.fromString('1440'))
 })
 
-test.todo('should interpret an implicit 1 preceding a high-timeframe resolution')
+test('hours should return associated integer times numbers', t => {
+    t.is(120, session.fromString('2H'))
+    t.is(240, session.fromString('4H'))
+    t.is(720, session.fromString('12H'))
+    t.is(1440, session.fromString('24H'))
+})
+
+test('days should return associated integer times numbers', t => {
+    t.is(2880, session.fromString('2D'))
+    t.is(10080, session.fromString('7D'))
+})
+
+test('weeks should return associated integer times numbers', t => {
+    t.is(20160, session.fromString('2W'))
+})
+
+test('months should return associated integer times numbers', t => {
+    t.is(604800, session.fromString('2M'))
+})
+
+test('years should return associated integer times numbers', t => {
+    t.is(7257600, session.fromString('2Y'))
+})
+
+test('should interpret an implicit 1 preceding a high-timeframe resolution', t => {
+    t.is(60, session.fromString('H'))
+    t.is(1440, session.fromString('D'))
+    t.is(10080, session.fromString('W'))
+    t.is(302400, session.fromString('M'))
+    t.is(3628800, session.fromString('Y'))
+})
